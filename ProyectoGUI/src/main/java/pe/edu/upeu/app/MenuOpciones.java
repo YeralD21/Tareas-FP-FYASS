beac5b07fb1ef80e273877e7f99e76833f283b7a
YeralD21 <yesarbot14@gmail.com>
2022-06-23 09:39:33 -0500

    Transformada01

--- ProyectoGUI/src/main/java/pe/edu/upeu/app/MenuOpciones.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/app/MenuOpciones.java
@@ -1,5 +1,6 @@
 package pe.edu.upeu.app;
 
+import java.io.CharArrayWriter;
 import java.io.Console;
 
 import pe.edu.upeu.modelo.UsuarioTO;
@@ -17,6 +18,7 @@ public class MenuOpciones {
         System.out.println("Ingrese su clave"); 
         char[] clavex=cons.readPassword();
         uTO.clave=String.valueOf(clavex);
+
         if (uTO.usuario.equals("yeraldss")&& uTO.clave.equals("123456")) {
             menuPrincipal();
         }else{
@@ -27,6 +29,27 @@ public class MenuOpciones {
     }
 
     public void menuPrincipal() {
-        
+        System.out.println("-----------------------Sistema de Ventas---------------------");
+        int opciones=1;
+        String menu="";
+        menu+="Registro de Productos\n";
+        menu+="Regristro de Ventas\n";
+        System.out.println(menu);
+        do {
+
+            switch (opciones) {
+                case 1: System.out.println("Registro Productos"); break;
+            
+                default: System.out.println("Opcion Invalida!"); break;
+            }
+            String continuar=leerT.leer("",
+             "Desea Continuar ? S=Si/N=No").charAt(0)=='S'?"S":"N";
+             if (continuar.charAt(0)=='S') {
+                opciones=leerT.leer(0, "Elija una operacion:\n"+menu);
+             }else{opciones=0;}
+        } while (opciones!=0);
+
+
+        System.out.println(); 
     }
 }
