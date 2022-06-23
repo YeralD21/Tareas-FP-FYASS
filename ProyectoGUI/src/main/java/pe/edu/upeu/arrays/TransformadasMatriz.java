432ba9f41649bb033be667e040d627e765fea37e
YeralD21 <yesarbot14@gmail.com>
2022-06-23 10:04:20 -0500

    Transformada12

--- ProyectoGUI/src/main/java/pe/edu/upeu/arrays/TransformadasMatriz.java
+++ ProyectoGUI/src/main/java/pe/edu/upeu/arrays/TransformadasMatriz.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class TransformadasMatriz {
     Scanner cs=new Scanner(System.in);
 
-    public void transformadas01() {
+    public void transformada01() {
         System.out.println("Ingrese Dimension Matriz:");
         int dimension=cs.nextInt();
         System.out.println("Ingrese valor de Inicio:");
@@ -20,21 +20,42 @@ public class TransformadasMatriz {
             }
         }
         imprimirMatrices(matriz);
-
     }
 
+    //--------------Transformada12
+    public void Transformada12() {
+        System.out.println("Ingrese Dimension Matriz:");
+        int dimension=cs.nextInt();
+        System.out.println("Ingrese valor de Inicio:");
+        int valorInit=cs.nextInt();  
+        String[][] matriz=new String[dimension][dimension];
+        for (int f = 0; f < matriz.length; f++) {
+            for (int c = matriz[0].length-1; c>=(0+f) ; c--) {
+                matriz[f][c]=String.valueOf(valorInit);
+                valorInit++;
+            }
+        }
+        imprimirMatrices(matriz);
+    }
+    //--------------
     public void imprimirMatrices(String[][] m) {
         for (int f = 0; f < m.length; f++) {
             for (int c = 0; c < m[0].length; c++) {
-                System.out.print(m[f][c]+"\t");               
+                if (m[f][c]==null) {
+                    System.out.print("\t");
+                }else{
+                    System.out.print(m[f][c]+"\t");  
+
+                }        
             }
-            System.out.println("");
+           System.out.println("");
         }
         
     }
 
 
     public static void main(String[] args) {
-        new TransformadasMatriz().transformadas01();
+        //new TransformadasMatriz().transformada01();
+        new TransformadasMatriz().Transformada12();
     }
 }
